c73b0c816b89d25831244d65c6564f68bd3013bd
StanislavSlobodenyuk <116057509+StanislavSlobodenyuk@users.noreply.github.com>
2023-01-24 23:11:19 +0200

    Added blocks and finished with markup
    Added: contents strategies, price table, reviews and get this blocks

--- app/js/main.js
+++ app/js/main.js
@@ -8,3 +8,59 @@ if (iconMenu) {
         menu.classList.toggle('_active');
     });
 }
+
+let contentsStrategies_Slider = new Swiper('.contents-strategies__swiper',{
+
+
+    // Кнопки пагінації 
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    // увімкнути/вимкнути захват та рух слайдів мишею на ПК
+    simulateTouch: true,
+    // чутливість
+    touchRatio: 1,
+    // кут роботи властивості
+    touchAngle: 45,
+    // курсор при переміщені
+    grabCursor: true,
+    // перемикання при кліку на слайд
+    slideToClickedSlide: true,
+  
+
+    // управління клавіатурою 
+    keyboard: {
+        // ввімкнути/вимкнути 
+        enabled: true,
+        // ввімкнути тільки тоді коли слайдер видно
+        onlyInViewport: true,
+        // ввімкнути керування за допомогою page up/down
+        pageUpDown: true,
+    },
+    // кількість слайдів для показу читатти там відключення функціоналу якщо слайдів менше чим порібно 
+    slidesPerView: 3,
+    watchOverflow: true,
+
+    // кількісь слайдів для листання
+    slidesPerGroup: 1,
+
+    // відступи між слайдами
+    spaceBetween: 32,
+
+    // швидкість перемикання слайдів
+    speed: 1000,
+    
+    breakpoints: {
+        320: {
+            slidesPerView: 1,
+        },
+        665: {
+            slidesPerView: 2,
+        },
+        992: {
+            slidesPerView: 3,
+        },
+    }
+});
+
